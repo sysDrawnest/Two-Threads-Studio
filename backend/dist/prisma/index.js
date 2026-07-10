db243c1e49de6059dc2b58ac917a397e6fadd6bc
SYS <118187096+sysDrawnest@users.noreply.github.com>
2026-07-10 15:26:57 +0530

    updated  .env file with the real Supabase PostgreSQL connection string!

--- backend/dist/prisma/index.js
+++ backend/dist/prisma/index.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const env_1 = require("../config/env");
+const pg_1 = require("pg");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const connectionString = env_1.env.DATABASE_URL;
+const pool = new pg_1.Pool({ connectionString });
+const adapter = new adapter_pg_1.PrismaPg(pool);
+const prisma = new client_1.PrismaClient({ adapter });
 exports.default = prisma;
--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client';
+import { env } from '../config/env';
 
-const prisma = new PrismaClient();
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
