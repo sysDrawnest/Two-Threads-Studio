25e2c2573aaf173eec2e8a243107ac488b23e0de
SYS <118187096+sysDrawnest@users.noreply.github.com>
2026-07-11 18:04:09 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -46,7 +46,7 @@ const Checkout = lazy(() => import('./pages/Checkout'));
 const Membership = lazy(() => import('./pages/Membership'));
 const Careers = lazy(() => import('./pages/Careers'));
 const Legal = lazy(() => import('./pages/Legal'));
-const OurBrands = lazy(() => import('./pages/OurBrands'));
+// const OurBrands = lazy(() => import('./pages/OurBrands'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 // Lazy Auth Pages
@@ -95,7 +95,7 @@ const StorefrontRoutes = () => {
         <Route path="/membership" element={<Membership />} />
         <Route path="/careers" element={<Careers />} />
         <Route path="/legal" element={<Legal />} />
-        <Route path="/our-brands" element={<OurBrands />} />
+        {/* <Route path="/our-brands" element={<OurBrands />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </AnimatePresence>
